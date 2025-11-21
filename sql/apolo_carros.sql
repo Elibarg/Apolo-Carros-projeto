0d30ba1090963286ee2b69b9ec733179911fb34b
Elibarg <calagabriel31@gmail.com>
2025-11-21 17:57:26 -0300

    sql

--- sql/apolo_carros.sql
+++ sql/apolo_carros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/11/2025 às 17:35
+-- Tempo de geração: 21/11/2025 às 21:56
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -41,6 +41,7 @@ CREATE TABLE `usuarios` (
   `estado` varchar(2) DEFAULT NULL,
   `cidade` varchar(100) DEFAULT NULL,
   `endereco` text DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
   `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp(),
   `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -51,12 +52,12 @@ CREATE TABLE `usuarios` (
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome_completo`, `email`, `senha`, `tipo_usuario`, `avatar_url`, `data_nascimento`, `genero`, `cpf`, `cep`, `estado`, `cidade`, `endereco`, `telefone`, `data_cadastro`, `data_atualizacao`, `ativo`) VALUES
-(1, 'Elga', 'admin@apolocarros.com', '$2y$10$K/ZhmrfYVGpRMunQgbiXse4O230BeMhLyr3tK3EBN5HHUJVhVXR7G', 'admin', '/Apolo-Carros-projeto/img/avatars/avatar_1_1761160008.png', NULL, NULL, '', '', '', '', '', '47992820417', '2025-10-22 18:41:20', '2025-10-23 00:22:08', 1),
-(2, 'João Silva Santos', 'joao.silva@email.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'usuario', '/Apolo-Carros-projeto/img/avatars/user1.jpg', '1990-05-15', 'male', '987.654.321-00', '04567-890', 'SP', 'São Paulo', 'Rua Augusta, 500 - Consolação', '(11) 98877-6655', '2025-10-22 18:41:20', '2025-10-22 18:41:20', 1),
-(3, 'Maria Oliveira Souza', 'maria.oliveira@email.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'usuario', '/Apolo-Carros-projeto/img/avatars/user2.jpg', '1988-08-20', 'female', '111.222.333-44', '22010-000', 'RJ', 'Rio de Janeiro', 'Avenida Atlântica, 2000 - Copacabana', '(21) 97766-5544', '2025-10-22 18:41:20', '2025-10-22 18:41:20', 1),
-(4, 'Pedro Costa Lima', 'pedro.costa@email.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'usuario', NULL, '1995-12-10', 'male', '555.666.777-88', '30110-000', 'MG', 'Belo Horizonte', 'Avenida do Contorno, 3000 - Centro', '(31) 96655-4433', '2025-10-22 18:41:20', '2025-10-22 18:41:20', 1),
-(9, 'GABRIEL ELESBA', 'calagabriel3441@gmail.com', '$2y$10$zd4Drvl4i7LeV.JwwluMk..3Rg8X63UM5WbsaY0pDkESnFIwPsxiG', 'usuario', NULL, '2006-03-22', 'male', '140.574.449-98', '89213-430', 'SC', 'Joinville', 'Rua Bom Retiro', '47992820423', '2025-11-16 21:04:23', '2025-11-16 22:00:00', 1);
+INSERT INTO `usuarios` (`id`, `nome_completo`, `email`, `senha`, `tipo_usuario`, `avatar_url`, `data_nascimento`, `genero`, `cpf`, `cep`, `estado`, `cidade`, `endereco`, `bairro`, `telefone`, `data_cadastro`, `data_atualizacao`, `ativo`) VALUES
+(1, 'Elga', 'admin@apolocarros.com', '$2y$10$K/ZhmrfYVGpRMunQgbiXse4O230BeMhLyr3tK3EBN5HHUJVhVXR7G', 'admin', '/Apolo-Carros-projeto/img/avatars/avatar_1_1761160008.png', NULL, NULL, '', '', '', '', '', NULL, '47992820417', '2025-10-22 18:41:20', '2025-10-23 00:22:08', 1),
+(2, 'João Silva Santos', 'joao.silva@email.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'usuario', '/Apolo-Carros-projeto/img/avatars/user1.jpg', '1990-05-15', 'male', '987.654.321-00', '04567-890', 'SP', 'São Paulo', 'Rua Augusta, 500 - Consolação', NULL, '(11) 98877-6655', '2025-10-22 18:41:20', '2025-10-22 18:41:20', 1),
+(3, 'Maria Oliveira Souza', 'maria.oliveira@email.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'usuario', '/Apolo-Carros-projeto/img/avatars/user2.jpg', '1988-08-20', 'female', '111.222.333-44', '22010-000', 'RJ', 'Rio de Janeiro', 'Avenida Atlântica, 2000 - Copacabana', NULL, '(21) 97766-5544', '2025-10-22 18:41:20', '2025-10-22 18:41:20', 1),
+(4, 'Pedro Costa Lima', 'pedro.costa@email.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'usuario', NULL, '1995-12-10', 'male', '555.666.777-88', '30110-000', 'MG', 'Belo Horizonte', 'Avenida do Contorno, 3000 - Centro', NULL, '(31) 96655-4433', '2025-10-22 18:41:20', '2025-10-22 18:41:20', 1),
+(9, 'GABRIEL ELESBA', 'calagabriel3441@gmail.com', '$2y$10$FTEW29wk2ds3TRvzZnaif.XDeqP3GQL3dcuC6FbHVPMSO/0h5HJW.', 'usuario', NULL, '2006-03-22', 'male', '140.574.449-98', '89213-430', 'SC', 'Joinville', 'Rua Bom Retiro', NULL, '47992820423', '2025-11-16 21:04:23', '2025-11-21 20:35:36', 1);
 
 -- --------------------------------------------------------
 
@@ -77,18 +78,19 @@ CREATE TABLE `veiculos` (
   `descricao` text DEFAULT NULL,
   `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp(),
   `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `ativo` tinyint(1) DEFAULT 1
+  `ativo` tinyint(1) DEFAULT 1,
+  `destaque` enum('sim','nao') DEFAULT 'nao'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Despejando dados para a tabela `veiculos`
 --
 
-INSERT INTO `veiculos` (`id`, `marca`, `modelo`, `ano`, `km`, `preco`, `status`, `data_compra`, `images`, `descricao`, `data_cadastro`, `data_atualizacao`, `ativo`) VALUES
-(1, 'Chevrolet', 'Onix LT', 2021, 95000, 75000.00, 'available', NULL, '[\"/Apolo-Carros-projeto/img/Chevrolet_Onix.png\"]', 'Sedan econômico, revisado.', '2025-11-17 09:40:30', '2025-11-17 09:40:30', 1),
-(2, 'Chevrolet', 'h423', 2000, 200, 20000000.00, 'sold', '2025-11-01', '[\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691aeffd050bc5.33996938.jpg\",\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691af37e0dc097.17956944.webp\"]', '', '2025-11-17 09:50:53', '2025-11-18 10:58:34', 1),
-(3, 'Sedan X3', 'Sedan X3', 2222, 400, 45000.00, 'sold', '2025-11-01', '[\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691af3a942a218.83267900.jpg\",\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691af3a94ddb78.87749105.webp\"]', '', '2025-11-17 10:06:33', '2025-11-19 16:21:43', 1),
-(4, 'h42398', 'h42398', 8888, 786, 67999.00, 'available', NULL, '[]', '', '2025-11-19 09:37:17', '2025-11-19 10:55:18', 1);
+INSERT INTO `veiculos` (`id`, `marca`, `modelo`, `ano`, `km`, `preco`, `status`, `data_compra`, `images`, `descricao`, `data_cadastro`, `data_atualizacao`, `ativo`, `destaque`) VALUES
+(1, 'Chevrolet', 'Onix LT', 2021, 95000, 75000.00, 'sold', '2025-10-11', '[\"\\/Apolo-Carros-projeto\\/img\\/Chevrolet_Onix.png\"]', 'Sedan econômico, revisado.', '2025-11-17 09:40:30', '2025-11-19 20:51:43', 1, 'sim'),
+(2, 'Chevrolet', 'h423', 2000, 200, 22.00, 'reserved', NULL, '[\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691aeffd050bc5.33996938.jpg\",\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691af37e0dc097.17956944.webp\"]', '', '2025-11-17 09:50:53', '2025-11-21 10:10:01', 1, 'sim'),
+(3, 'Sedan X3', 'Sedan X3', 2222, 400, 45000.00, 'available', NULL, '[\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691af3a942a218.83267900.jpg\",\"\\/Apolo-Carros-projeto\\/img\\/vehicles\\/veh_691af3a94ddb78.87749105.webp\"]', '', '2025-11-17 10:06:33', '2025-11-21 10:09:46', 1, 'sim'),
+(4, 'h42398', 'h42398', 8888, 786, 67999.00, 'sold', '2025-08-22', '[]', '', '2025-11-19 09:37:17', '2025-11-19 20:53:08', 1, 'sim');
 
 --
 -- Índices para tabelas despejadas
